530b754d1a6d32b38033b2aeb52449b78d72249e
Aldrin <axzt2004@gmail.com>
2024-11-17 21:46:56 -0400

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductosComponent } from './paginas/productos/productos.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'productos' , component: ProductosComponent},
+    {path: 'contacto' , component: ContactoComponent}
+];
